ce2ab936f4ebcc2402e5af5d454117b415b0652c
Slavik <slavik-skripka@yandex.ru>
2022-07-15 18:54:53 +0300

    2 version

---
+++ Homework_6_seminar/Who/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 з
+// адаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите 1 число: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3 число: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 4 число: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double result = (b2 - b1)/(k1 - k2);
+
+Console.Write("Точка пересечения: " + result);
---
+++ One lessons/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+
+//Console.WriteLine("Вот наш массив: ");
+//PrintArray(numbers);
+int sum = 0;
+
+for (int z = 0; z < numbers.Length; z+=2)
+    sum = sum + numbers[z];
+
+    Console.WriteLine($"Всего {numbers.Length} чисел, сумма элементов на нечётных позициях = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    Console.WriteLine("Вот наш массив: ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1,10);
+        Console.Write($"[ {numbers[i]} ]");
+        //Console.Write(numbers[i] + "\t");
+    }
+    Console.WriteLine();
+}
+// void PrintArray(int[] numbers)
+// {
+//     Console.Write("[ ");
+//     for(int i = 0; i < numbers.Length; i++)
+//         {
+//             Console.Write(numbers[i] + " ");
+//         }
+//     Console.Write("]");
+//     Console.WriteLine();
+// }
