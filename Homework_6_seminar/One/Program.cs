e7565cf8f005c6d02d46ca4171ee9bb664f71cb9
Slavik <slavik-skripka@yandex.ru>
2022-07-15 18:52:35 +0300

    1 version

---
+++ Homework_6_seminar/One/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+//  сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите числа: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] arrayNumber = new int [number];
+int count = 0;
+
+for (int i = 0; i < arrayNumber.Length; i++)
+{
+    arrayNumber[i] = Convert.ToInt32(Console.ReadLine());
+    if (arrayNumber[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Чисел больше 0 было введено: {count}");
+
